5a3af6b3f9a1a1aa52d3a468f62eb312073f7ccc
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-30 08:58:04 +0530

    added: return total fare

---
+++ src/test/java/com/testcabinvoicegenrator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,26 @@
+package com.testcabinvoicegenrator;
+import com.cabinvoicegenrator.CabInvoiceGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CabInvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 5.0;
+        double time = 2;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        Assertions.assertEquals(52, totalfare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenLessThanMinimumFare_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        double time = 2;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        Assertions.assertEquals(5, totalfare,0.0);
+    }
+}
